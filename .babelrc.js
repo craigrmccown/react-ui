56a4acce2e22a5798a8237002ca1dd8ebda2e68a
Craig McCown <craigrmccown@gmail.com>
2019-03-17 16:14:05 -0400

    Add library boilerplate with Babel configuration

---
+++ .babelrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  presets: [['@babel/env', { modules: 'commonjs' }], '@babel/react'],
+}
---
+++ src/components/Hello/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hello = () => (
+  <div>
+    <span role="img" aria-label="wave">
+      👋
+    </span>
+    Hello, world!
+  </div>
+)
+
+export default Hello
